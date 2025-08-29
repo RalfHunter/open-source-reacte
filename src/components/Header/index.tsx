72268b9c3a085dbab3d56e8e631c11aa59b9574c
Silvio Huan Matheus Grein Cristo Ribeiro <silviohuan@gmail.com>
2025-08-28 22:15:56 -0400

    Primeiro Commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+export default function Header() {
+    return (
+        <header>
+            <div className='header'>
+                <h1 className='titulo'>Open Source Projects</h1>
+                <img className='fundo' src="../../../public/notebook.png" alt="" />
+            </div>
+        </header>
+    )
+}
